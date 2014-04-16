d55c5c52a1c5ead112a1ddc14d2b4794b888db36
Sam Richard <snugug@gmail.com>
2014-04-16 09:14:06 -0400

    JSON Building

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,8 +4,15 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var paths = require('compass-options').paths();
+var dirs = require('compass-options').dirs();
 var browserSync = require('browser-sync');
 var shell = require('gulp-shell');
+
+
+var walk = require('walk');
+var jf = require('jsonfile');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
 
 //////////////////////////////
 // Begin Gulp Tasks
@@ -25,7 +32,7 @@ gulp.task('lint', function () {
 gulp.task('compass', function () {
   return gulp.src(paths.sass + '/**/*')
     .pipe(shell([
-      'bundle exec compass watch --time'
+      'compass watch --time'
     ]));
 });
 
@@ -33,8 +40,27 @@ gulp.task('compass', function () {
 // Watch
 //////////////////////////////
 gulp.task('watch', function () {
-  gulp.watch(paths.js + '/**/*.js', ['lint']);
+  watch({ glob: dirs.js + '/**/*.js'},
+    function (files) {
+      return files.pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+    }
+  );
 });
+// gulp.task('watch', function () {
+//   watch({
+//     glob: paths.js + '/**/*.js'
+//   })
+//     .pipe(jshint())
+//     .pipe(jshint.reporter(stylish))
+//   // gulp.src(paths.js + '/**/*.js')
+//   //   .pipe(watch(function (files) {
+//   //     return files.pipe(lint());
+//   //   }));
+
+//   // gulp.watch(paths.js + '/**/*.js', ['lint']);
+//   // gulp.watch(paths.html + '/components/**/*.html', ['walk']);
+// });
 
 //////////////////////////////
 // BrowserSync Task
@@ -58,3 +84,44 @@ gulp.task('browserSync', function () {
 //////////////////////////////
 gulp.task('server', ['watch', 'compass', 'browserSync']);
 gulp.task('serve', ['server']);
+
+//////////////////////////////
+// Default Task
+//////////////////////////////
+gulp.task('default', ['server']);
+
+//////////////////////////////
+// Walk
+//////////////////////////////
+gulp.task('walk', function () {
+  var files = [];
+  var walker = walk.walk('components');
+
+  walker.on('file', function(root, stat, next) {
+    var fileName = stat.name;
+        fileName = fileName.replace('.html', '');
+    var filePath = root + '/' + stat.name;
+    var fileRoot = root.replace('components', '');
+        fileRoot = fileRoot.charAt(0) === '/' ? fileRoot.substr(1) : fileRoot;
+    var file = {
+      "name": fileName,
+      "path": filePath,
+      "root": fileRoot
+    };
+
+    files.push(file);
+    next();
+  });
+
+  walker.on('end', function () {
+    jf.writeFile('./data/page.json', files, function(err) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log('Wrote page.json');
+      }
+    });
+  });
+
+});
