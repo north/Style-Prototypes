ad417d8d06810335aad71b5f4056b267bccffd1e
Sam Richard <snugug@gmail.com>
2014-04-22 16:59:37 -0400

    Put reading into a try/catch to not totally fail

--- sp/menu.js
+++ sp/menu.js
@@ -10,16 +10,24 @@ module.exports.buildMenu = function (options) {
     var sections = Object.keys(jf.readFileSync('.www/config/sections.json'));
 
     var menu = [];
+    var files = {};
 
     sections.forEach(function (k) {
-      var path = '.www/data/'+ k + '.json';
+      var path = '.tmp/data/'+ k + '.json';
       if (fs.existsSync(path)) {
-        var data = jf.readFileSync(path);
-        menu.push(data.menu[0]);
+        try {
+          var data = jf.readFileSync(path);
+          menu.push(data.menu[0]);
+          files[k] = data.files;
+        }
+        catch (e) {
+          console.log(e);
+        }
       }
     });
 
     jf.writeFileSync('.www/config/menu.json', menu);
+    jf.writeFileSync('.www/config/files.json', files);
 
     return cb(null, gulpFile);
   });
