ce12c2148cebf2cf865d9c3bc793d0b10164dcc2
Sam Richard <snugug@gmail.com>
2014-04-28 15:13:12 -0400

    Build Single JS file from All SP Source

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,23 @@
 'use strict';
 
 var gulp = require('gulp');
-require('./sp/sp.js')(gulp);
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('compress', function () {
+  gulp.src([
+    'bower_components/angular/angular.js',
+    'bower_components/angular-route/angular-route.js',
+    'bower_components/angular-local-storate/angular-local-storage.js',
+    'build/templates.js',
+    'build/factories.js',
+    'build/directives.js',
+    'build/app.js'
+  ])
+  .pipe(concat('style-prototype.js'))
+  .pipe(uglify({
+    mangle: true,
+    outSourceMap: true
+  }))
+  .pipe(gulp.dest('dist'));
+});
