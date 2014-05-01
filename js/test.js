7b0f744b645b54dbc135a4ef55263a74d33ad9e0
Sam Richard <snugug@gmail.com>
2014-05-01 17:53:42 -0400

    HTML Source

--- js/test.js
+++ js/test.js
@@ -1,13 +1,31 @@
 (function () {
   'use strict';
 
+  function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
   window.addEventListener('DOMContentLoaded', function () {
-    var base = document.getElementById('base--obj');
-    console.log('foo');
-    if (base) {
-      console.log(base);
-      base.style.color = 'red';
+    var partials = document.querySelectorAll('[data-sp-class="section--header"] ~ span[ng-include]');
+
+    for (var i in partials) {
+      if (!partials[i].nextSibling) {
+        var source = partials[i].innerHTML;
+        if (source) {
+          source = source.replace(/<!--/g, '\n<!--').replace(/-->/g, '-->\n');
+          var sourceContainer = document.createElement('pre');
+          sourceContainer.setAttribute('data-sp-class', 'source--html');
+          var sourceView = document.createElement('code');
+          sourceView.setAttribute('class', 'language-markup');
+          sourceView.textContent = source;
+          sourceContainer.appendChild(sourceView);
+
+          insertAfter(partials[i], sourceContainer);
+        }
+      }
     }
+
+    // partials.innerHTML.replace(/</g, '&lt').replace(/>/g, '&gt')
 
   });
 })();
